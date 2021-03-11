d2875103044dcb9df4aadc974578eecd68d3891f
Danny Tran <dannyytran@gmail.com>
2021-03-11 11:23:07 -0800

    downloadUrl

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,14 +1,11 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:wasteagram/widgets/widgets.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:share/share.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
+import 'dart:io';
 
 class DetailScreen extends StatefulWidget {
   State createState() => _DetailScreenState();
@@ -23,15 +20,14 @@ class _DetailScreenState extends State<DetailScreen> {
     image = File(pickedFile.path);
 
     try {
-      await FirebaseStorage.instance.ref(basename(image.path)).putFile(image);
-      // Reference storageReference =
-      //     FirebaseStorage.instance.ref().child('test.jpg');
-      // await storageReference.putFile(image);
+      Reference uploadReference =
+          FirebaseStorage.instance.ref(basename(image.path));
+      await uploadReference.putFile(image);
+      String downloadUrl = await uploadReference.getDownloadURL();
+      
     } on FirebaseException catch (error) {
       print(error);
     }
-
-    // final downloadurl = firebaseStorageInstance.ref().getDownloadURL();
     setState(() {});
   }
 

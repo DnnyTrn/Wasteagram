affe1c0af95731ce5e350199ec14fba364ca2c7a
Danny Tran <dannyytran@gmail.com>
2021-03-11 18:53:10 -0800

    displays food

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,60 +1,29 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:wasteagram/widgets/widgets.dart';
-import 'package:share/share.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
-import 'dart:io';
+import 'package:wasteagram/models/food.dart';
 
 class DetailScreen extends StatefulWidget {
   static String routeName = 'DetailScreen';
   State createState() => _DetailScreenState();
 }
 
+// The Detail Screen should display the post's date, photo, number of wasted items,
+// and the latitude and longitude that was recorded as part of the post.
 class _DetailScreenState extends State<DetailScreen> {
   Widget build(BuildContext context) {
+    final Food food = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(),
-      body: Text('details'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text('${food.created}'),
+            Text('${food.imageUrl}'),
+            Text('Items: ${food.quantity}'),
+            Text('(${food.location})'),
+          ],
+        ),
+      ),
     );
   }
-  // File image;
-
-  // void pickImage() async {
-  //   PickedFile pickedFile =
-  //       await ImagePicker().getImage(source: ImageSource.gallery);
-  //   if (pickedFile == null) return; //user canceled image pick
-  //   try {
-  //     image = File(pickedFile.path);
-  //     Reference uploadReference =
-  //         FirebaseStorage.instance.ref(basename(image.path));
-  //     await uploadReference.putFile(image);
-  //     String downloadUrl = await uploadReference.getDownloadURL();
-  //   } on FirebaseException catch (error) {
-  //     print(error);
-  //   }
-  //   setState(() {});
-  // }
-
-  // void shareImage() async {
-  //   await Share.shareFiles([image.path]);
-  //   // Share.share('check out my website https://example.com');
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   if (image == null) {
-  //     return GetImageButton(pickImage: pickImage);
-  //   } else {
-  //     return Center(
-  //       child: Column(
-  //         children: [
-  //           Center(child: Image.file(image)),
-  //           ShareImageButton(shareImage: shareImage),
-  //         ],
-  //       ),
-  //     );
-  //   }
-  // }
 }
